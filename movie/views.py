8e69636c321ead8a4926e485a755377b7164216d
QuitoTactico <teteban0917@gmail.com>
2024-02-07 10:11:04 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -8,4 +8,5 @@ def home(request):
     return render(request, 'home.html', {'name':'Esteban Vergara Giraldo'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
