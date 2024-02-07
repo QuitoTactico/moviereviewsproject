8bbec76d6025e434b16cc0db9fb56052a55f8b67
QuitoTactico <teteban0917@gmail.com>
2024-02-07 10:32:33 -0500

    Modelo para Movie y requirements para el proyecto

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,13 @@
+# Necesitas instalar pillow
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    # Preguntar diferencia entre CharField y TextField
+    description = models.CharField(max_length=250)
+    # Dice en dónde se guardarán las imágenes montadas por la gente
+    image = models.ImageField(upload_to='movies/images/')
+    # Es un campo opcional
+    url = models.URLField(blank=True)
