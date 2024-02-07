37fd0ec91f8a9f9ef3def58ec044572b4e4bdecf
QuitoTactico <teteban0917@gmail.com>
2024-02-07 11:54:37 -0500

    De nuevo el nombre rojo sale en pantalla

--- movie/views.py
+++ movie/views.py
@@ -21,7 +21,7 @@ def home(request):
     else:
         movies = Movie.objects.all()
 
-    return render(request, 'home.html', {'searchTerm' : searchTerm,'movies':movies})
+    return render(request, 'home.html', {'name': 'Esteban Vergara Giraldo','searchTerm' : searchTerm,'movies':movies})
 
 
 def about(request):
