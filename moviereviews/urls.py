c50edbe77527a5add29676890a1470a2a3b2e9a3
QuitoTactico <teteban0917@gmail.com>
2024-02-07 10:51:37 -0500

    Podemos agregar películas ahora desde admin, las imágenes se guardan

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
+# Nuevo
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
     path('about/', movieViews.about),
 ]
+
+# Nuevo
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
